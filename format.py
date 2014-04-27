bf55d679891bd229accbeed5a5f8eda54666a56b
Warwick Stone <uozu.aho@gmail.com>
2014-04-27 22:42:52 +1000

    [ADD] Initial files
    Doesn't do anything yet, but has a keyboard shortcut!

---
+++ format.py
@@ -0,0 +1,19 @@
+import sublime
+import sublime_plugin
+
+
+PLUGIN_NAME = u"SublimeFormatShortcut"
+
+
+def load_settings():
+    return sublime.load_settings(PLUGIN_NAME + '.sublime-settings')
+
+
+def get_setting(key):
+    return load_settings().get(key)
+
+
+class FormatCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        print("format shortcut: not implemented...")
