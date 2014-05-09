e8ef73707b71a6b0e4c9082995d87d10b3a7db5c
Warwick Stone <uozu.aho@gmail.com>
2014-05-09 23:55:25 +1000

    [ADD] format selection

--- format.py
+++ format.py
@@ -25,10 +25,26 @@ def get_format_command(syntax):
     return get_setting('syntax_formatter_mapping').get(syntax)
 
 
+def get_syntax(view_syntax_string):
+    # shameless rip from CodeFormatter. Cheers akalongman!
+    pattern = re.compile(r"Packages/(.+?)/.+?\.tmLanguage")
+    m = pattern.search(view_syntax_string)
+    found = ""
+    if (m):
+        for s in m.groups():
+            found = s
+            break
+    return found.lower()
+
+
+def test():
+    sublime.Window.new_file()
+
+
 class SublimeFormatShortcutCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
-        syntax = self.get_view_syntax()
+        syntax = get_syntax(self.view.settings().get('syntax'))
         dbg("current view syntax: " + syntax)
         format_command = get_format_command(syntax)
         if format_command is None:
@@ -38,13 +54,19 @@ class SublimeFormatShortcutCommand(sublime_plugin.TextCommand):
             dbg(syntax + " formatting command: " + format_command)
             self.view.run_command(format_command)
 
-    def get_view_syntax(self):
-        # shameless rip from CodeFormatter. Cheers akalongman!
-        pattern = re.compile(r"Packages/(.+?)/.+?\.tmLanguage")
-        m = pattern.search(self.view.settings().get('syntax'))
-        found = ""
-        if (m):
-            for s in m.groups():
-                found = s
-                break
-        return found.lower()
+
+class FormatShortcutSelectionCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        sel = self.view.sel()
+        region1 = sel[0]
+        selectionText = self.view.substr(region1)
+        tmp = self.view.window().new_file()
+        tmp.insert(edit, 0, selectionText)
+        tmp.set_syntax_file(self.view.settings().get('syntax'))
+        tmp.run_command('sublime_format_shortcut')
+        newtxt = tmp.substr(sublime.Region(0, tmp.size()))
+        self.view.replace(edit, region1, newtxt)
+        tmp.set_scratch(True)
+        self.view.window().focus_view(tmp)
+        self.view.window().run_command("close_file")
