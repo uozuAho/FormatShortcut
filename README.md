b40f0393b735c53e03d99c56323999e31cc03790
Warwick Stone <uozu.aho@gmail.com>
2014-05-09 20:20:39 +1000

    Now working. Added a number of supported syntaxes.

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 SublimeFormatShortcut
 =====================
 
-One hotkey to format any filetype in Sublime Text
+One command/shortcut to format any syntax in Sublime Text.
+Chooses a formatter based on current syntax highlighting setting.
+
+
+Configuration
+-------------
+Syntaxes are mapped to formatters in the settings file, under the
+setting "syntax_formatter_mapping". Change this mapping as you please -
+formatters not included (install separately).
+
+
+TODO
+----
+- Format selection
+- Format as another syntax
+- Auto-install necessary formatter plugins?
+- Support standalone formatters (not sublime plugins)
--- format.py
+++ format.py
@@ -1,8 +1,16 @@
 import sublime
 import sublime_plugin
+import re
 
 
 PLUGIN_NAME = u"SublimeFormatShortcut"
+
+DEBUG = True
+
+
+def dbg(msg):
+    if DEBUG:
+        print("format shortcut: " + msg)
 
 
 def load_settings():
@@ -13,8 +21,30 @@ def get_setting(key):
     return load_settings().get(key)
 
 
-class FormatCommand(sublime_plugin.TextCommand):
+def get_format_command(syntax):
+    return get_setting('syntax_formatter_mapping').get(syntax)
+
+
+class SublimeFormatShortcutCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
-        print("format shortcut: not implemented...")
-        self.view.run_command('pep8_autoformat')
+        syntax = self.get_view_syntax()
+        dbg("current view syntax: " + syntax)
+        format_command = get_format_command(syntax)
+        if format_command is None:
+            sublime.error_message(
+                PLUGIN_NAME + ": no formatter set for syntax: " + syntax)
+        else:
+            dbg(syntax + " formatting command: " + format_command)
+            self.view.run_command(format_command)
+
+    def get_view_syntax(self):
+        # shameless rip from CodeFormatter. Cheers akalongman!
+        pattern = re.compile(r"Packages/(.+?)/.+?\.tmLanguage")
+        m = pattern.search(self.view.settings().get('syntax'))
+        found = ""
+        if (m):
+            for s in m.groups():
+                found = s
+                break
+        return found.lower()
